641180578083451a9083649d189db16d764f5cb8
Jorge Colmenarez <jlct.master@gmail.com>
2025-07-02 12:29:17 -0400

    Add log msg

--- net.frontuari.bpallocation/src/net/frontuari/bpallocation/webui/apps/form/Allocation.java
+++ net.frontuari.bpallocation/src/net/frontuari/bpallocation/webui/apps/form/Allocation.java
@@ -1312,11 +1312,12 @@ public class Allocation extends CustomForm
 							+ "FROM C_Invoice WHERE C_Invoice_ID=?";
 				}
 				BigDecimal open = DB.getSQLValueBD(trxName, sql, C_Invoice_ID);
+				log.warning("Saldo Factura = "+open+" desde sql = "+sql);
 				if (open != null && open.signum() == 0)	 {
 					sql = "UPDATE C_Invoice SET IsPaid='Y' "
 						+ "WHERE C_Invoice_ID=" + C_Invoice_ID;
 					int no = DB.executeUpdate(sql, trxName);
-					if (log.isLoggable(Level.CONFIG)) log.config("Invoice #" + i + " is paid - updated=" + no);
+					if (log.isLoggable(Level.WARNING)) log.warning("Invoice #" + i + " is paid - updated=" + no);
 				} else {
 					if (log.isLoggable(Level.CONFIG)) log.config("Invoice #" + i + " is not paid - " + open);
 				}
